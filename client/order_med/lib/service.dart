4f4573e72c46fc7ebbb68042d0913a2d17cab724
Ayush Shashank <ayush.bibhas@gmail.com>
2022-10-11 16:21:01 +0530

    Custom Fetch API

---
+++ client/order_med/lib/service.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart' as http;
+
+class Service {
+  static String baseUrl = 'http://localhost:3000';
+
+  static Future<dynamic> fetch(String path, {Object? body}) async {
+    final uri = Uri.parse("$baseUrl$path");
+    var headers = {
+      "Accept": "application/json",
+      "Access-Control_Allow_Origin": "*"
+    };
+
+    http.Response response;
+    if (body == null) {
+      response = await http.get(uri, headers: headers);
+    } else {
+      response = await http.post(uri, headers: headers, body: body);
+    }
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response, then parse the JSON.
+      print(response.body);
+      return response.body;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to fetch');
+    }
+  }
+}
