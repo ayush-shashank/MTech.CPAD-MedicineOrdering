062d042ebf74a84c9a915568bb98b110f8746e55
Ayush Shashank <ayush.bibhas@gmail.com>
2022-10-19 03:32:06 +0530

    Card UI Updates

--- client/order_med/lib/widgets/order_card.dart
+++ client/order_med/lib/widgets/order_card.dart
@@ -10,66 +10,87 @@ class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () => Navigator.pushNamed(context, '/product'),
       child: Card(
-        child: Padding(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+            decoration: BoxDecoration(
+              // color: Colors.amber,
+              border: Border(
+                  top: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 4.0,
+              )),
+            ),
             padding: const EdgeInsets.fromLTRB(8, 2, 8, 8),
             child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(
-                      'Order # ${order.id}',
-                      style: const TextStyle(
-                        fontStyle: FontStyle.italic,
-                        fontSize: 10,
+                    Expanded(
+                      child: Text(
+                        'Order # ${order.id}',
+                        textAlign: TextAlign.right,
+                        style: const TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontSize: 10,
+                        ),
                       ),
                     ),
                   ],
                 ),
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Column(children: [
-                      Image.network(
-                          '${globals.baseUrl}/assets/img/${order.productId}/0.jpg',
-                          fit: BoxFit.contain,
-                          height: 100,
-                          width: 100,
-                          errorBuilder: (BuildContext context, Object exception,
-                                  StackTrace? stackTrace) =>
-                              const Center(
-                                child: Icon(
-                                  Icons.warning_amber_rounded,
-                                  size: 100,
-                                  color: Colors.amber,
-                                ),
-                              ))
-                    ]),
+                    Expanded(
+                        flex: 1,
+                        child: Image.network(
+                            '${globals.baseUrl}/assets/img/${order.productId}/0.jpg',
+                            fit: BoxFit.contain,
+                            semanticLabel: 'Image',
+                            errorBuilder: (BuildContext context,
+                                    Object exception, StackTrace? stackTrace) =>
+                                const Center(
+                                  child: Icon(
+                                    Icons.warning_amber_rounded,
+                                    size: 100,
+                                    color: Colors.amber,
+                                  ),
+                                ))),
                     const SizedBox(
                       width: 8,
+                      height: 8,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(order.productName,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.w700, fontSize: 16)),
-                        Text(
-                          order.updatedAt.toString(),
-                          style: const TextStyle(fontStyle: FontStyle.italic),
-                        ),
-                        Text(
-                          'Quantity: ${order.quantity}',
-                          style: const TextStyle(fontSize: 16),
-                        )
-                      ],
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(order.productName,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 16)),
+                          Text(
+                            order.updatedAt.toString(),
+                            style: const TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                          Text(
+                            'Quantity: ${order.quantity}',
+                            style: const TextStyle(fontSize: 16),
+                          )
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ],
             )),
       ),
-      onTap: () => Navigator.pushNamed(context, '/product'),
     );
   }
 }
